0e47c539c4c69dbb239e5bba26eb68e83d98b1fa
gabriel-gibran <gabrielgibran37@gmail.com>
2023-02-10 01:11:30 -0800

    done

---
+++ js/teacher-orders.js
@@ -0,0 +1,31 @@
+function acceptscience () {
+    alert('Hoorey, you accepted it!');
+    document.getElementById('orderscience').style.display = 'none';
+}
+
+function acceptmath () {
+    alert('Hoorey, you accepted it!');
+    document.getElementById('ordermath').style.display = 'none';
+}
+
+function acceptenglish () {
+    alert('Hoorey, you accepted it!');
+    document.getElementById('orderenglish').style.display = 'none';
+    
+}
+
+
+function rejectscience () {
+    alert('you cancelled the order :(');
+    document.getElementById('orderscience').style.display = 'none';
+}
+
+function rejectmath () {
+    alert('you cancelled the order :(');
+    document.getElementById('ordermath').style.display = 'none';
+}
+
+function rejectenglish () {
+    alert('you cancelled the order :(');
+    document.getElementById('orderenglish').style.display = 'none';
+}
